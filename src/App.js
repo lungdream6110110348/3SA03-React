e177031a912599dc1083665fb1d3674703dd4a74
lungdream6110110348 <lungdream6110110348@gmail.com>
2020-08-15 05:01:46 +0700

    design app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import WordCard from './WordCard';
 const word = "AABBCCDDEEFF";
 function App() {
   return (
-    <body><h1>Welcome to my Game!</h1>
+    <body ><h1>Welcome to my Game!</h1>
+          <h2>Try to match all the same Alphabet! </h2>
     <div>
       <WordCard value={word}/>
     </div>
